5c917611d12e593acbc075be1971d8ec7974666d
Joseph Petroske <jmpetroske@gmail.com>
2018-05-18 16:30:50 -0700

    changed acceptable methods

--- src/server/web.go
+++ src/server/web.go
@@ -64,7 +64,7 @@ func startHandler(w http.ResponseWriter, r *http.Request) {
 func commandHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	w.Header().Add("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE")
+	w.Header().Add("Access-Control-Allow-Methods", "POST")
 	w.Header().Add("Access-Control-Expose-Headers", "Authorization")
 	w.Header().Add("Access-Control-Max-Age", "600")
 	
