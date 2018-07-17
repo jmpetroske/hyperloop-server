c5cee5fb41583a888b9759ca56402390c6875b51
Joseph Petroske <jmpetroske@gmail.com>
2018-07-16 21:08:22 -0700

    readded vincent's mode

--- src/server/main.go
+++ src/server/main.go
@@ -15,6 +15,7 @@ func main() {
 	var tcpOnly *bool = flag.Bool("tcp-only", false, "Use tcp for all communications")
 	var logData *bool = flag.Bool("log-data", false,
 		"Log data as it comes in from the teensy. Data will be printed to stdout")
+	var vincentTest *bool = flag.Bool("vincent-testing", false, "vincent's mode")
 	flag.Parse()
 
 	var wg sync.WaitGroup
@@ -27,7 +28,10 @@ func main() {
 
 	wg.Add(1)
 	go func() {
-		if *tcpOnly {
+		if *vincentTest {
+			log.Println("Using test mode for vincent")
+			startFakeArduino()
+		} else if *tcpOnly {
 			log.Println("Using only tcp for communications")
 			tcpComs(true)
 		} else {
